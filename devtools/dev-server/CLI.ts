d217cbc89e05802bb6e4544e43f786890ed63757
Alex Figliolia <alexfigliolia@gmail.com>
2024-03-10 10:45:47 -0700

    Migrate to shared dev server

--- devtools/dev-server/CLI.ts
+++ devtools/dev-server/CLI.ts
@@ -1,5 +1,13 @@
-import { Development } from "./Development";
+import { DevServer } from "@figliolia/typescript-dev-server";
 
 (async () => {
-  await Development.run();
+  const Server = new DevServer({
+    entryPoint: "src/Start.ts",
+    nodeOptions: {
+      NODE_ENV: "development",
+      NODE_OPTIONS: "--enable-source-maps",
+      NODE_EXTRA_CA_CERTS: "../my-perf-core/cert/server.cert",
+    },
+  });
+  await Server.run();
 })().catch(console.log);
